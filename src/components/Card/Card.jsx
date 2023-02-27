04e4b0326fe19c817e43d67d19102aa3eba87851
Bryce Schultz <bryceschultz26@gmail.com>
2023-02-26 22:42:23 -0500

    fixing card image display on safari

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,8 +7,8 @@ const Card = ({image, title, company, highlightedWork}) => {
     return (
         <>
         <div className="card-container w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow ">
-        <div className="flex flex-col items-start items-center">
             <img className="card-image" src={image} />
+            <div className="flex flex-col items-center">
             <span className="text-sm text-gray-500 dark:text-gray-400">{title}</span>
             <div className="flex mt-4 space-x-3 md:mt-6">
                 <button onClick={() => setShowModal(true)} className="w-full inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">See More</button>
